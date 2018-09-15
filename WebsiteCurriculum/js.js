11c5cfb9c7a286c891c86c2e190bc3116cf037a7
Diogo Sousa <42503667+Diogo12246@users.noreply.github.com>
2018-09-15 06:08:56 +0100

    Update js.js

--- WebsiteCurriculum/js.js
+++ WebsiteCurriculum/js.js
@@ -1,6 +1,8 @@
-function copiarEmail() {
-    var Email = "diogo12246@gmail.com"
-    Email.select();
-    document.execCommand("copy");
-    alert("Contacto copiado para o seu clipboard: " + Email);
+//function copiarEmail() {
+ //   var Email = "diogo12246@gmail.com"
+   // Email.select();
+    //document.execCommand("copy");
+    //alert("Contacto copiado para o seu clipboard: " + Email);
   }
+
+//outdated
